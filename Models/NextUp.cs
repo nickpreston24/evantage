33b25fc21eca4cbf1e13e5c73b2d1786dd92f6e0
mikepreston17 <michael.n.preston@gmail.com>
2024-01-07 21:15:57 -0600

    ++ packages and css

---
+++ Models/NextUp.cs
@@ -0,0 +1,5 @@
+namespace evantage.Models;
+
+public class NextUp
+{
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,53 @@
+using System.Reflection;
+using CodeMechanic.Embeds;
+using CodeMechanic.FileSystem;
+using CodeMechanic.RazorHAT.Services;
+
+var policyName = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Allow CORS: https://www.stackhawk.com/blog/net-cors-guide-what-it-is-and-how-to-enable-it/#enable-cors-and-fix-the-problem
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: policyName,
+        builder =>
+        {
+            builder
+                // .WithOrigins("http://localhost:3000", "tpot-links-mkii")
+                .AllowAnyOrigin()
+                .WithMethods("GET")
+                .AllowAnyHeader();
+        });
+});
+
+// Load and inject .env files & values
+DotEnv.Load();
+
+
+builder.Services.AddTransient<IEmbeddedResourceQuery, EmbeddedResourceQuery>();
+builder.Services.AddSingleton<IMarkdownService, MarkdownService>();
+
+builder.Services.AddControllers();
+
+
+var main_assembly = Assembly.GetExecutingAssembly();
+builder.Services.AddSingleton<IEmbeddedResourceQuery>(
+    new EmbeddedResourceService(
+            new Assembly[]
+            {
+                main_assembly
+            },
+            debugMode: false
+        )
+        .CacheAllEmbeddedFileContents());
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+// source: https://github.com/tutorialseu/sending-emails-in-asp/blob/main/Program.cs
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -14,6 +58,7 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(policyName);
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -21,5 +66,9 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();

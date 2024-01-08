d853a68eaf6601d8afba3ca1a8af12c76995883d
mikepreston17 <michael.n.preston@gmail.com>
2024-01-07 22:21:15 -0600

    ++ added installment plan values

--- Models/Commissions.cs
+++ Models/Commissions.cs
@@ -5,6 +5,13 @@ public class Commissions
     public List<Line> Lines { get; set; } = new();
     public List<Phone> Phones { get; set; } = new();
 
-    public double Total { get; set; } = -1.00;
-    public string Post { get; set; } = "📱📱2xNU";
+    // public double Total { get; set; } = -5.00;
+    public double Total()
+    {
+        double total = -5.00;
+
+        return total;
+    }
+
+    public string Post { get; set; } = "📱📱2xNU (sample)";
 }
--- Models/InstallmentPlan.cs
+++ Models/InstallmentPlan.cs
@@ -4,6 +4,18 @@ namespace evantage.Models;
 
 public class InstallmentPlan : Enumeration
 {
+    public double GetEarnings()
+    {
+        var earning_lookup = new Dictionary<InstallmentPlan, double>()
+        {
+            { NextUp, 10 },
+            { Other, 0 },
+        };
+
+        var _ = earning_lookup.TryGetValue(this, out var found);
+        return found;
+    }
+
     public static InstallmentPlan NextUp => new(1, nameof(NextUp).ToLower());
     public static InstallmentPlan Other => new(2, nameof(Other).ToLower());
 

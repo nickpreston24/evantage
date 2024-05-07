3e83107eb2f27c33c5c40b5b94b853ba88952597
mikepreston17 <michael.n.preston@gmail.com>
2024-05-07 09:50:42 -0500

    ++ 2 card flip samples; my fav is flipcard-tut with it upsplash

---
+++ Doodles/CardFlips/card-flip/main.js
@@ -0,0 +1,9 @@
+// const card = document.querySelector(".card__inner");
+
+// card.addEventListener("click", function (e) {
+//   card.classList.toggle("is-flipped");
+// });
+
+// card.addEventListener("onmouseover", function (e) {
+//   card.classList.toggle("is-flipped");
+// });
---
+++ Doodles/CardFlips/flipcard-tut/README.md
@@ -0,0 +1,7 @@
+# flipcard-tut
+ Flipcard tutorial in HTML and CSS
+
+[Live Preview](https://htmlpreview.github.io/?https://github.com/russs123/flipcard-tut/blob/main/index.html)
+
+
+[Source](https://github.com/russs123/flipcard-tut)
